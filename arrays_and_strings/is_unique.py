cb350d377ddae7e478289a51aa0d20281205a9d6
Camila Andrea Gonzalez Williamson <camilaagw@gmail.com>
2021-05-03 21:33:09 +0200

    Add solution with a bit vector

--- arrays_and_strings/is_unique.py
+++ arrays_and_strings/is_unique.py
@@ -1,18 +1,29 @@
 """Are all characters in a string unique?"""
 
 
-def main(my_string: str) -> bool:
-
+def is_unique(my_string: str) -> bool:
+    """Using a hash-set"""
     my_set = set()
     for char in my_string:
-        if char not in my_set:
-            my_set.add(char)
-        else:
+        if char in my_set:
             return False
+        my_set.add(char)
+    return True
 
+
+def is_unique_v1(my_string: str) -> bool:
+    """Using a bit vector :)"""
+    checker = 0
+    for char in my_string:
+        position = ord(char) - 97
+        if (checker & (1 << position)) > 0:
+            return False
+        checker |= (1 << position)
     return True
 
 
 if __name__ == "__main__":
-    assert not main("aasssasfsdfdfdf")
-    assert main("abcdegf")
+
+    for func in [is_unique, is_unique_v1]:
+        assert not func("aasssasfsdfdfdf")
+        assert func("abcdegf")
