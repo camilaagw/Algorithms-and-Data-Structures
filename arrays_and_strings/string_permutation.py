ee41dc0be1ad0aa7ce73a4630dde3b4dc143267d
Camila Andrea Gonzalez Williamson <camilaagw@gmail.com>
2021-05-03 21:33:38 +0200

    Refactor with Counter

--- arrays_and_strings/string_permutation.py
+++ arrays_and_strings/string_permutation.py
@@ -1,29 +1,22 @@
 """Find if a string is a permutation of another string"""
 
-from collections import Counter, defaultdict
+from collections import Counter
 
 
-def is_permutation(string1, string2):
-
+def is_permutation(string1: str, string2: str) -> bool:
     return Counter(string1) == Counter(string2)
 
 
-def is_permutation_alt(string1, string2):
-    letters_map = defaultdict(int)
-
-    for char in string1:
-        letters_map[char] += 1
+def is_permutation_alt(string1: str, string2: str) -> bool:
+    """Using just one counter (less space)"""
+    letters_map = Counter(string1)
 
     for char in string2:
         if char not in letters_map or letters_map[char] == 0:
             return False
         letters_map[char] -= 1
 
-    for char in letters_map:
-        if letters_map[char] != 0:
-            return False
-
-    return True
+    return sum(letters_map.values()) == 0
 
 
 if __name__ == "__main__":
